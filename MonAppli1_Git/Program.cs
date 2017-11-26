a9992bf0f7feda4b85535a9ef7d0487104f012b4
SG <sophie.girard@ac-grenoble.fr>
2017-11-26 08:05:52 +0100

    Première validation Main

--- MonAppli1_Git/Program.cs
+++ MonAppli1_Git/Program.cs
@@ -10,6 +10,8 @@ namespace MonAppli1_Git
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
