cd8049592fd91fe975f38f80c12ec99d822486ed
SG <sophie.girard@ac-grenoble.fr>
2017-11-27 20:19:11 +0100

    Ajout classe etudiant

---
+++ MonAppli1_Git/Etudiant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonAppli1_Git
+{
+    public class Etudiant : Personne
+    {
+        private string diplome;
+        public Etudiant(int unId, string unNom, string unDiplome):base(unId, unNom)
+        {
+            this.diplome = unDiplome;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " - Diplôme = "+ this.diplome;
+        }
+    }
+}
